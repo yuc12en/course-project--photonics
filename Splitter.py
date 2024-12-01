95aa007823ed024b01b9e7f850a0f17bbf624f9f
yuchen <yuc12en@gmail.com>
2024-12-01 15:04:28 -0500

    wrap the z0 as a function of L and h0

--- Splitter.py
+++ Splitter.py
@@ -1,29 +1,40 @@
-from Coupler_parameters import *
 from eigen_value import *
 
+wavelength = 1.5
+k0 = 2*np.pi/wavelength
+nf = 3.5
+ns = 1.5
+nc = 1.5
 
-x, E, out = solution_patterns(wavelength, h0, top_width, bottom_width, nf, ns)
+mu = 1.257e-6
+w = k0*3e8
 
-L = 1  # displacement 2L
-x1 = x
-E1 = E[0]
-x2 = x-2*L
-E2 = E1
+def get_z0(L, k0, h0, top_width, bottom_width, nf, ns):
+    x, E, out = solution_patterns(k0, h0, top_width, bottom_width, nf, ns)
+    x1 = x
+    E1 = E[0]
+    x2 = x-2*L
+    E2 = E1
 
-wg_bottom_2 = wg_bottom-2*L
-wg_top_2 = wg_top-2*L
+    wg_bottom = -h0/2
+    wg_top = h0/2
 
-E2 = E2[(x2>wg_bottom_2) & (x2<wg_top_2)]
-x2 = x2[(x2>wg_bottom_2) & (x2<wg_top_2)]
-E1 = E1[(x1>wg_bottom_2) & (x1<wg_top_2)]
-x1 = x1[(x1>wg_bottom_2) & (x1<wg_top_2)]
+    wg_bottom_2 = wg_bottom-2*L
+    wg_top_2 = wg_top-2*L
 
-# fig, ax = plt.subplots()
-# ax.plot(x1, E1, label='x1')
-# ax.plot(x2, E2, label='x2')
-# ax.legend()
-# plt.show()
+    E2 = E2[(x2>wg_bottom_2) & (x2<wg_top_2)]
+    x2 = x2[(x2>wg_bottom_2) & (x2<wg_top_2)]
+    E1 = E1[(x1>wg_bottom_2) & (x1<wg_top_2)]
+    x1 = x1[(x1>wg_bottom_2) & (x1<wg_top_2)]
 
-kappa = w*mu/4 * (nf**2-ns**2) * simpson(E1*E2, x=x1)
-print("Kappa: {}".format(kappa))
-print("z0 :{}".format(np.pi/4/kappa))
+    # plt.plot(x2,E2)
+    # plt.show()
+
+    kappa = w*mu/4 * (nf**2-ns**2) * simpson(E1*E2, x=x1)
+    z0 = np.pi*4/kappa
+    return kappa, z0
+
+if __name__ == '__main__':
+    L = 1.6
+    kappa, z0 = get_z0(L, k0, h0=3, top_width=18.5,bottom_width=18.5, nf=3.5, ns=1.5)
+    print(kappa, z0)
