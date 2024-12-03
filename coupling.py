b08f904bc6301e4aaf80a88030f46fce7c678158
yuchen <yuc12en@gmail.com>
2024-12-03 18:47:27 -0500

    source degenerate coupling. Try another method next version

--- coupling.py
+++ coupling.py
@@ -50,7 +50,7 @@ def intermode_coupling(k0, h0, length, slope1, z_turn, slope2, z_interval, dx):
         if (z_interval*slope1/2 <= dx*3) or (z_interval*slope2/2 <= dx*3):  # At three x points in the calculation
             raise ValueError("Too sparse x. Increase slope, z_interval or reduce dx")
 
-        indx = (x>h_low/2) & (x>h_high/2)
+        indx = (x>h_low/2) & (x<h_high/2)
 
         x_step = x[indx]
         E1_step = An[indx]
@@ -86,19 +86,20 @@ def source_coupling(k0, h0, length, slope, z_interval, dx):
     ns = 1.5
     nf = 3.5
 
+    h_tip = z_interval*slope
 
     iterations = int(length/z_interval)
     if z_interval*slope/2 <= dx*3:
         raise ValueError("Too sparse x. Increase slope, z_interval or reduce dx")
 
-    out = solve_eigen(k0, h0, nf, ns, ns)
+    out = solve_eigen(k0, h_tip, nf, ns, ns)
     x = np.arange(-20,20,dx)
 
     A = np.exp(-(x/5)**2)
-    beta_A = nf*k0
-    An, C_A = normalized_distribution(x, A, w, mu, beta_A, return_type='coeff')   #  set the beta_i = k0*nf
+    # beta_A = nf*k0
+    # An, C_A = normalized_distribution(x, A, w, mu, beta_A, return_type='coeff')   #  set the beta_i = k0*nf
 
-    B = generate_mode_function(out['kf'][0],out['ys'][0], h0, mode=0)(x)
+    B = generate_mode_function(out['kf'][0],out['ys'][0], h_tip, mode=0)(x)
     beta_B = out['beta'][0]
     Bn, C_B = normalized_distribution(x, B, w, mu, beta_B, return_type='coeff')
     C_B = 0
@@ -107,29 +108,60 @@ def source_coupling(k0, h0, length, slope, z_interval, dx):
     Change_CA = []
     Change_CB = []
     for i in range(iterations):
-        h_low = h0 + z_interval*i*slope
-        h_high = h0 + z_interval*(i+1)*slope
+        h_low = z_interval*i*slope
+        h_high = z_interval*(i+1)*slope
 
-        indx = (x>h_low/2) & (x>h_high/2)
+        indx = (x>h_low/2) & (x<h_high/2)
 
         x_step = x[indx]
-        E1_step = An[indx]
+        E1_step = A[indx]
         E2_step = Bn[indx]
 
-        kappa = epsilon*w/4* (ns**2-nf**2) *(simpson(E1_step*E2_step, x=x_step))*2
 
-        delta_A = 1j*kappa*C_B*Bn*np.exp(-1j*(beta_A-beta_B)*z_interval/2)*z_interval
-        delta_B = 1j* np.conjugate(kappa)*C_A*An *np.exp(1j*(beta_A-beta_B)*z_interval/2)*z_interval
+        dBdz = -1j*w/4 *epsilon*(nf**2-ns**2)* simpson(E1_step*E2_step, x=x_step)
+        dAdz = -1j*w/4 *epsilon*(nf**2-ns**2)* simpson(E1_step*E1_step, x=x_step)
 
-        A = C_A*An + delta_A
-        B = C_B*Bn + delta_B
+        # fig, ax = plt.subplots()
+        # ax.plot(x_step, E1_step)
+        # ax.plot(x_step, E2_step)
+        # plt.show()
 
-        An, C_A = normalized_distribution(x, A, w, mu, beta_A, return_type='coeff')
+        # kappa = epsilon*w/4* (ns**2-nf**2) *(simpson(E1_step*E2_step, x=x_step))*2
+        # delta_A = 1j*kappa*C_B*Bn*np.exp(-1j*(beta_A-beta_B)*z_interval/2)*z_interval
+        # delta_B = 1j* np.conjugate(kappa)*C_A*An *np.exp(1j*(beta_A-beta_B)*z_interval/2)*z_interval
+        # A = A + delta_A
+        # B = C_B*Bn + delta_B
+
+        A = A + dAdz * z_interval
+        B = C_B*Bn + dBdz * z_interval
+
+        loopnum = 3
+        # if i < loopnum:
+            # fig, ax = plt.subplots(1,2)
+            # ax[0].plot(x, A)
+            # ax[1].plot(x_step,E1_step)
+            # ax[1].plot(x_step,E2_step)
+            # plt.show()
+
+            # print(max(A))
+            # print(dBdz)
+            # print('\n')
+        # if i==loopnum:
+            # break
+
+        # An, C_A = normalized_distribution(x, A, w, mu, beta_A, return_type='coeff')
+
         Bn, C_B = normalized_distribution(x, B, w, mu, beta_B, return_type='coeff')
-        Change_CA.append(C_A)
         Change_CB.append(C_B)
+        Change_CA.append(max(A))
 
-    return ([[An, C_A], [Bn, C_B]], Change_CA, Change_CB)
+        next_h = z_interval*(2*i+1)*slope/2
+        out = solve_eigen(k0, next_h, nf, ns, ns)
+        B = generate_mode_function(out['kf'][0],out['ys'][0], next_h, mode=0)(x)
+        beta_B = out['beta'][0]
+        Bn = normalized_distribution(x, B, w, mu, beta_B)
+
+    return ([[A], [Bn, C_B]], Change_CA, Change_CB)
 
 
 
@@ -162,27 +194,31 @@ if __name__ == '__main__':
     # dx = z_interval*np.min([slope1, slope2])/2/4
 
     # AB, As, Bs = intermode_coupling(k0, h0, 1000, slope1, 500, slope2, z_interval, dx)
-    # # Change of the amplititude of higher mode B
+    # Change of the amplititude of higher mode B
     # fig, ax = plt.subplots()
     # ax.plot(range(len(Bs)), Bs, 'k--')
     # ax2 = ax.twinx()
     # ax2.plot(range(len(As)), As, 'k-')
     # plt.show()
 
-    slope = 0.01
-    z_interval = 1
+    length = 0.2
+    slope = 1
+    z_interval = 0.01
     dx = z_interval*slope/2/4
-    AB, As, Bs = source_coupling(k0, h0, 1000, slope, z_interval, dx)
+
+    AB, As, Bs = source_coupling(k0, h0, length, slope, z_interval, dx)
     fig, ax = plt.subplots()
-    ax.plot(range(len(Bs)), Bs, 'k--', label='A')
-    ax.set_ylabel('B')
+    print(As[:10])
 
-    ax2 = ax.twinx()
-    ax2.plot(range(len(As)), As, 'k-', label='B')
-    ax2.set_ylabel('A')
+    # ax.plot(range(len(Bs)), np.abs(Bs), 'k--', label='B')
+    # ax.set_ylabel('B')
 
-    plt.legend()
-    plt.show()
+    # ax2 = ax.twinx()
+    # ax2.plot(range(len(As)), np.abs(As), 'k-', label='A')
+    # ax2.set_ylabel('A')
+
+    # plt.legend()
+    # plt.show()
 
 
 
